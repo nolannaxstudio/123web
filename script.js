e75c89b566e75d2703fbd28c96e4d176e498e3e4
nolannaxstudio <166512738+nolannaxstudio@users.noreply.github.com>
2026-01-12 15:29:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,2 @@
-function toggleMenu() {
-  document.getElementById("menu").classList.toggle("show");
-}
+// Pour le moment, pas besoin de JS pour le scroll, le CSS scroll-behavior: smooth; gère tout.
+// Tu peux ajouter des effets plus avancés ici si tu veux.
